7dd078a64b705803a7fbcaf27dbaa623dedd5c0a
Arash Shahry <arash.shahry@gmail.com>
2015-08-27 17:09:45 -0400

    Add seeds

---
+++ config/seeds.js
@@ -0,0 +1,46 @@
+var DB = require("../config/connection")
+var Student = DB.models.Student
+var Lesson = DB.models.Lesson
+var Instructor = DB.models.Instructor
+var Comment = DB.models.Comment
+
+var students = [
+  {username:"Arash", password:"alpha"},
+  {username:"Erica", password:"beta"},
+  {username:"Justin", password:"gamma"},
+  {username:"Adrian",  password:"delta"}
+]
+   
+
+var lessons = [
+  {topic:"AJAX", date: new Date(), time:"morning"},
+  {topic:"APIs", date: new Date(), time:"afternoon"},
+  {topic:"HTML", date: new Date(), time:"morning"},
+  {topic:"CSS", date: new Date(), time:"afternoon"},
+  {topic:"jQuery", date: new Date(), time:"morning"},
+  {topic:"Git", date: new Date(), time:"afternoon"}
+]
+
+var instructors = [
+  {username:"abray", password:"abcdefgh", name:"Adam Bray", photo_url:"www.google.com"},
+  {username:"jesse", password:"abcdefgh", name:"Jesse Shawl", photo_url:"www.google.com"},
+  {username:"andy", password:"abcdefgh", name:"Andy Kim", photo_url:"www.google.com"},
+  {username:"adrian", password:"abcdefgh", name:"Adrian Maseda", photo_url:"www.google.com"},
+  {username:"matt", password:"abcdefgh", name:"Matt Scilipoti", photo_url:"www.google.com"},
+  {username:"robin", password:"abcdefgh", name:"Robin Thomas", photo_url:"www.google.com"}
+]
+
+var comments = [
+  {request_type:"help", content:"Slow down!"},
+  {request_type:"help", content:"Gotta pee!"},
+  {request_type:"help", content:"Ur killin' it!"},
+  {request_type:"comment",  content:"What page are we on?"}
+]
+
+Lesson.bulkCreate(lessons).then(function(){
+  return Comment.bulkCreate(comments)
+})
+.then(function(){
+  console.log("Seeded successfully! kthxbye");
+  process.exit();
+});
