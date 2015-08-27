0e26b0df0774528e7e714be8cffc502ed2bb9f9b
Erica Irving <contact.eirving@gmail.com>
2015-08-27 11:25:43 -0400

    more de-twittering

--- controllers/lessons.js
+++ controllers/lessons.js
@@ -6,14 +6,14 @@ var Lesson = DB.models.Lesson;
 function error(response, message){
   response.status(500);
   response.json({error: message});
-};
+}
 
 // Index
 router.get("/lessons", function(req, res) {
-  console.log(["ID", req.session.profile.id])
+  // console.log(["ID", req.session.profile.id]);
   Lesson.findAll().then(function(lessons){
-    console.log(["I GOT THIS MANY LESSONS", lessons.length])
-    res.json({lessons: lessons, user: req.session.profile});
+    console.log(["I GOT THIS MANY LESSONS", lessons.length]);
+    // res.json({lessons: lessons, user: req.session.profile});
   });
 });
 
@@ -53,10 +53,10 @@ router.delete("/lessons/:id", function(req, res){
   Lesson.findById(req.params.id)
   .then(function(lesson){
     if(!lesson) return error(res, "not found");
-    return lesson.destroy()
+    return lesson.destroy();
   })
   .then(function(lesson){
-    res.json(lesson)
+    res.json(lesson);
   });
 });
 
