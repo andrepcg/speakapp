e47296e3339c9d3c7c7b1480086dfb7458fee0ea
Erica Irving <contact.eirving@gmail.com>
2015-08-27 10:36:55 -0400

    update twitter callback url

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 module.exports = {
   consumerKey: "UODWaL79S6UGKDxyZphtFbDSP",
   consumerSecret: "gGJtWJLpmv2EbK82ETH0YLR06N79gHePr9EZPvxHggJ0gWzBcE",
-  callbackUrl: "http://127.0.0.1:3001/auth/twitter/callback" //Change to localhost:3001?
-}
+  callbackUrl: "https://ga-speakapp.herokuapp.com/auth/twitter/callback"
+};
