978a14da7106e1206867715f6d366047a442dc78
Erica Irving <contact.eirving@gmail.com>
2015-08-27 10:11:16 -0400

    provision a db with test table

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 module.exports = {
   consumerKey: "UODWaL79S6UGKDxyZphtFbDSP",
   consumerSecret: "gGJtWJLpmv2EbK82ETH0YLR06N79gHePr9EZPvxHggJ0gWzBcE",
-  callbackUrl: "http://127.0.0.1:3001/auth/twitter/callback"
+  callbackUrl: "http://127.0.0.1:3001/auth/twitter/callback" //Change to localhost:3001?
 }
--- index.js
+++ index.js
@@ -2,47 +2,62 @@ var Express = require("express");
 var app = Express();
 var path = require("path");
 var bodyParser = require("body-parser");
-var env = require("./env")
-var session = require("express-session")
-var passport = require("passport")
-var TwitterStrategy = require("passport-twitter").Strategy
+var env = require("./env");
+var session = require("express-session");
+var passport = require("passport");
+var TwitterStrategy = require("passport-twitter").Strategy;
 passport.serializeUser(function(user, done) {
-  done(null, user)
-})
+  done(null, user);
+});
 passport.deserializeUser(function(obj, done) {
-  done(null, obj)
-})
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(session({secret: "banana"}))
+  done(null, obj);
+});
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(session({secret: "banana"}));
 
 passport.use(new TwitterStrategy({
   consumerKey: env.consumerKey,
   consumerSecret: env.consumerSecret,
   callbackUrl: env.callbackUrl
 }, function(aToken, aTokenSecret, aProfile, done){
-  token = aToken
-  tokenSecret = aTokenSecret
-  profile = aProfile
-  done(null, profile)
-}))
+  token = aToken;
+  tokenSecret = aTokenSecret;
+  profile = aProfile;
+  done(null, profile);
+}));
 
 app.get("/", function(req, res) {
-  res.redirect("/auth/twitter")
+  res.redirect("/auth/twitter");
 });
 
 app.get("/auth/twitter", passport.authenticate("twitter"), function(req, res){
-})
+});
+
 app.get("/auth/twitter/callback", passport.authenticate('twitter'), function(req, res){
- req.session.token = token
- req.session.tokenSecret = tokenSecret
- req.session.profile = profile
- res.redirect("/lessons")
-})
+ req.session.token = token;
+ req.session.tokenSecret = tokenSecret;
+ req.session.profile = profile;
+ res.redirect("/lessons");
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/", Express.static(path.join(__dirname + "/public")));
+
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+ pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+   client.query('SELECT * FROM test_table', function(err, result) {
+     done();
+     if (err)
+      { console.error(err); response.send("Error " + err); }
+     else
+      { response.render('pages/db', {results: result.rows} ); }
+   });
+ });
+});
 
 var lessonsController = require("./controllers/lessons");
 app.use("/", lessonsController);
